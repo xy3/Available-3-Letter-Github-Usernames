fe73707a5bf2882b1ea37d723171132448364c21
xy3 <a@xsq.pw>
2023-04-28 12:47:59 +0200

    add new availables

--- github_username_checker.py
+++ github_username_checker.py
@@ -2,23 +2,68 @@
 
 from multiprocessing import Pool
 import sys, requests
+import threading
+from datetime import datetime
+import tqdm
+
+def check(username):
+    status = request(username)
+    if status == 500:
+        return {"status": "unchecked", "username": username}
+
+    if status == 404:
+        return {"status": "available", "username": username}
+
+    return {"status": "taken", "username": username}
+
 
 def request(line):
-	user = line.strip()
-	status = requests.get(f"https://github.com/{user}").status_code
-	result = {"user": user, "data": status}
-	if status == 404: print(user)
-	return result
+    user = line.strip()
+    try:
+        result = requests.get(f"https://github.com/{user}", timeout=10)
+        return result.status_code
+    except Exception:
+        return 500
 
-# Set how many threads u want lol
-pool = Pool(processes=100)
 
-try:
-	lines = sys.stdin.readlines()
-	results = pool.map(request, lines)
-except KeyboardInterrupt:
-	sys.exit() # OH SHIT CTRL+C
+def main():
+    inputList = [s.strip() for s in sys.stdin.readlines()] 
+    unchecked = set(inputList)
+    available = []
+    taken = []
 
-# Can also print them out again
-# for res in results:
-# 	print(res)
+    pool = Pool(processes=50)
+
+    try:
+        for result in tqdm.tqdm(pool.imap_unordered(check, inputList), total=len(inputList)):
+            status = result["status"]
+            if status != "unchecked":
+                unchecked.remove(result["username"])
+            if status == "available":
+                available.append(result["username"])
+            else:
+               taken.append(result["username"])
+
+
+        now = datetime.now()
+        dl_string = now.strftime("%d_%m_%Y_%H:%M:%S")
+        with open(f"unchecked_{dl_string}.txt", "x") as f:
+            f.write("\n".join(list(unchecked)))
+            f.write("\n")
+
+        with open(f"available_{dl_string}.txt", "x") as f:
+            f.write("\n".join(available))
+            f.write("\n")
+ 
+        with open(f"taken_{dl_string}.txt", "x") as f:
+            f.write("\n".join(taken))       
+            f.write("\n")
+
+        print(f"\nAvailable: {len(available)} | Taken: {len(taken)} Unchecked/failed: {len(unchecked)}\n")
+
+    except KeyboardInterrupt:
+        sys.exit() # ctrl c 
+
+
+if __name__ == "__main__":
+    main()
